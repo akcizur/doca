0063eb0f019a7992cbc612a28a7de5b385f86c66
vics <ruzickajakub95@gmail.com>
2026-09-17 01:53:03 +0200

    Delete src components

--- src/components/ContactSection.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section id="kontakt" class="contact">
-    <div class="container">
-      <div class="section-label">POJĎME buildit</div>
-      <h2 class="section-title">MÁTE<br />PROJEKT?</h2>
-      <a href="mailto:mail@ruzickajakub.cz" class="contact-email">mail@ruzickajakub.cz</a>
-    </div>
-  </section>
-</template>
